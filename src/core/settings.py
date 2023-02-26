fa8893f0ccd4a6d638fb07f26b55e817619ed67f
Uralbek Yergenov <oralbekergenov@gmail.com>
2023-02-26 20:05:50 +0600

    Сделать конфигурацию

--- src/core/settings.py
+++ src/core/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'labwork12',
+        'USER': 'ural',
+        'PASSWORD': '1237894567oM',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 

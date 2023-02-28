303b3fc01d4f54068c42a563d8808743de910bdc
Uralbek Yergenov <oralbekergenov@gmail.com>
2023-02-28 13:33:50 +0600

    Добавить возможность создать продукт

--- src/products/forms.py
+++ src/products/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from .models import Product
+
 
 class SearchForm(forms.Form):
     title = forms.CharField(
@@ -13,3 +15,33 @@ class SearchForm(forms.Form):
             'id': 'title',
         })
     )
+
+
+class ProductCreateForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['title', 'category', 'image', 'price', 'amount', 'description']
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-control mb-3'
+            }),
+            'category': forms.Select(
+                choices=Product.CategoryChoices.choices,
+                attrs={
+                    'class': 'form-select mb-3'
+                }
+            ),
+            'image': forms.TextInput(attrs={
+                'class': 'form-control mb-3'
+            }),
+            'price': forms.NumberInput(attrs={
+                'class': 'form-control mb-3'
+            }),
+            'amount': forms.NumberInput(attrs={
+                'min': 0,
+                'class': 'form-control mb-3'
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'form-control mb-3'
+            })
+        }
--- src/products/views/product.py
+++ src/products/views/product.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from ..models import Product
-from ..forms import SearchForm
+from ..forms import SearchForm, ProductCreateForm
 
 
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
-    return render(request, 'products/product_detail.html', context={
+    return render(request, 'products/detail.html', context={
         'product': product,
     })
 
@@ -22,3 +22,21 @@ def product_search(request):
         'products': result,
     })
 
+
+def product_create(request):
+    match request.method:
+        case 'GET':
+            form = ProductCreateForm()
+            return render(request, 'products/create.html', context={
+                'form': form,
+            })
+
+        case 'POST':
+            form = ProductCreateForm(request.POST)
+            if not form.is_valid():
+                return render(request, 'products/create.html', context={
+                    'form': form,
+                })
+
+            Product.objects.create(**form.cleaned_data)
+            return redirect('index')

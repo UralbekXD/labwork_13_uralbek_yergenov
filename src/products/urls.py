a02eb5aad3af7c6db01244548c252358c753d6b5
Uralbek Yergenov <oralbekergenov@gmail.com>
2023-02-28 12:12:30 +0600

    Добавить возможность посмотреть все о продукте
     - Исправить путь к статическим файлам в корне проекта
     - Изменить высоту продукта на главной странице
     - Добавить шаблон для просматра конкретного продукта
     - Добавить представление просмотра продукта
     - Добавить стили к шаблону просмотра продукта
     - Добавить импорт в файле __init__ для product view
     - Добавить ссылку на продукт

--- src/products/urls.py
+++ src/products/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
 from .views import index
+from .views import product_detail
+
 
 urlpatterns = [
     path('', index, name='index'),
     path('products/', index, name='products'),
+    path('product/<int:pk>', product_detail, name='product_detail'),
 ]
--- src/products/views/index.py
+++ src/products/views/index.py
@@ -5,7 +5,6 @@ from ..models import Product
 
 def index(request):
     products = Product.objects.filter(amount__gt=0).order_by('category', 'title')
-    print(Product.CategoryChoices.choices)
     return render(request, 'products/index.html', context={
         'products': products,
         'categories': Product.CategoryChoices.choices,
